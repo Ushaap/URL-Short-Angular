d3b4b6e6ff456d5bdadfcd12765b0fdcc5277050
Ushaa P <ushaa96@gmail.com>
2020-04-27 19:32:04 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UrlgenshortComponent } from './urlgenshort/urlgenshort.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UrlgenshortComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule  
+
   ],
   providers: [],
   bootstrap: [AppComponent]
